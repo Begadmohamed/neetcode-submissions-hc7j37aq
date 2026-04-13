b737f689d558ee79c1d2358fcf343bccad6b02d4
Begad Mohamed <es-begad.mohamed2027@alexu.edu.eg>
2026-04-13 17:22:01 +0200

    Refactor lengthOfLongestSubstring method

--- Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
@@ -2,22 +2,15 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) < 2:
             return len(s)
-        
-        i = 0
-        j = 0
-        hashTable = {}
+        i=0
+        j=0
+        hashTable={}
         maxLen = 0
-        
-        while j < len(s):
-            char = s[j]
-            hashTable[char] = hashTable.get(char, 0) + 1
-            
-            while hashTable[char] > 1:
-                left_char = s[i]
-                hashTable[left_char] -= 1
-                i += 1
-            
-            maxLen = max(maxLen, j - i + 1)
-            j += 1
-            
+        while j<len(s):
+            hashTable[s[j]] = hashTable.get(s[j],0) + 1
+            while hashTable[s[j]] > 1:
+                hashTable[s[i]] -= 1
+                i+=1
+            maxLen=max(maxLen,j-i+1)
+            j+=1
         return maxLen
